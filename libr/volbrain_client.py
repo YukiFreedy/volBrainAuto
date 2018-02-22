7162947ae97a04e09627a2321b28c886c1782419
Rafa <rafag.1858@hotmail.es>
2018-02-22 17:03:06 +0100

    Había olvidado añadir los ficheros de los que hablaba en el commit anterior.

---
+++ libr/volbrain_client.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+from gui import Ui_MainWindow
+
+### VENTANA PRINCIPAL ###
+class volBrainClient(QtWidgets.QMainWindow):
+    
+    def __init__(self, parent=None):
+        super(volBrainClient, self).__init__(parent)
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    form = volBrainClient()
+    form.show()
+    app.exec_()
+    
+if __name__ == "__main__":
+    main()
